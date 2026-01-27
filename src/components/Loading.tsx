6f740902c393d0b742c829ea3651734885e8081f
ali said <aliasy19932017@gmail.com>
2026-01-27 06:25:39 +0200

    updated style.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="element-center h-screen">
+    <div className="element-center min-h-[calc(100vh-76px)]">
       <div className="animate-spin rounded-full h-14 w-14 border-2 border-t-primary" />
     </div>
   );
